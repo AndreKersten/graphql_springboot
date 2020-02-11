d535371ddf1e08d7b328b55226491a93d41d3a86
Andre Kersten <andre.kersten.de@gmail.com>
2020-02-11 14:38:12 +0100

    setup JUnit for testing with simple test

---
+++ src/test/java/de/kersten/test/GraphQLRouteQueryTest.java
@@ -0,0 +1,35 @@
+package de.kersten.test;
+
+import com.graphql.spring.boot.test.GraphQLResponse;
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+import static graphql.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class GraphQLRouteQueryTest {
+
+    @Autowired
+    private GraphQLTestTemplate graphQLTestTemplate;
+
+    @Test
+    public void routeQueryroutes() throws IOException { 
+        GraphQLResponse response  = graphQLTestTemplate.postForResource("routes.graphql");
+        assertNotNull(response);
+        assertTrue(response.isOk());
+        assertEquals("101", response.get("$.data.routes[0].id"));
+    }
+    
+     
+      
+}
